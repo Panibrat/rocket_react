bae191aac320ae2d90515ad923e55db407654f3d
Oleksandr_Panibratenko <Oleksandr_Panibratenko@epam.com>
2024-11-18 00:31:24 +0200

    remove https

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = 'http://192.168.1.4';
-export const BASE_URL = 'https://192.168.4.1';
+export const BASE_URL = 'http://192.168.4.1';
 // export const BASE_URL = 'https://my.backend/api';
 
 export const GET_DATA_URL = '/mesData';
