054d1bcec13c35f959d44a701ac07104559139e5
Oleksandr_Panibratenko <Oleksandr_Panibratenko@epam.com>
2024-11-17 23:45:14 +0200

    axios for https ssl ignore

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { FETCH_INTERVAL } from '../constants/times';
 import { BASE_URL, GET_DATA_URL } from '../constants/urls';
 
@@ -6,22 +7,30 @@ export const useData = () => {
   const [data, setData] = useState({ thrust: 0, state: 'R' });
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const fetchData = () => {
+
+  const fetchData = async () => {
     setIsLoading(true);
-    fetch(`${BASE_URL}${GET_DATA_URL}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then((response) => response.json())
-      .then((data) => setData(data))
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-        setError(error);
-      })
-      .finally(() => setIsLoading(false));
+    try {
+      const response = await axios.get(`${BASE_URL}${GET_DATA_URL}`, {
+        headers: { 'Content-Type': 'application/json' },
+        // Ігнорування SSL
+        validateStatus: () => true,
+        transformRequest: [
+          (data, headers) => {
+            headers['X-Ignore-SSL'] = 'true'; // Додаткові хедери за потреби
+            return data;
+          }
+        ]
+      });
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
   };
+
   useEffect(() => {
     const interval = setInterval(fetchData, FETCH_INTERVAL);
 
